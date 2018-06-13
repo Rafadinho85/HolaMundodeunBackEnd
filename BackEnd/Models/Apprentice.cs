4dd46143128a04695ddb3018b0004ee03b4ab543
Rafadinho <rafa85jogo@gmail.com>
2018-06-12 21:32:52 -0600

    Add project files.

---
+++ BackEnd/Models/Apprentice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd.Models
+{
+    //nuestro modelo siempre debemos marcarlo como publico sino podremos tener incovenientes en la implementacion
+    public class Apprentice
+    {
+        //trabajaremos con los campos o atributos que forman parte de la clase
+        //sino lo marcamos como publico por defecto se entiende que es privado
+        //public int id;
+
+        //encapsulamiento para proteger el acceso directo de nuestros datos
+
+        // metodo largo
+
+        //    //set para asignar
+        //public void Setid (int i)
+        //{
+
+        //    id = i;
+        //}
+        //// get para obtener un dato
+        //public int Getid ()
+        //{
+        //    return id;
+
+       // }
+
+      //  metodo corto
+      
+        public int Id
+        {
+            set;get;
+        }
+
+        
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+}
